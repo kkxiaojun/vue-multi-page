64d972d370cc58012dad3cc982bb4ea04b3cfb02
zhaoguojun <985531883@qq.com>
2020-09-02 18:39:25 +0800

    feat: update

--- src/pages/demo/views/home.vue
+++ src/pages/demo/views/home.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <img :src="loadingImg" alt="">
+    <div class="img-box" v-for="(item, index) in imgList" :key="index">
+      <img :src="item" loading="lazy" alt="demo">
+      <!-- <img loading="lazy" width="400" height="400" :src="item" alt="..." /> -->
+      <!-- <iframe :src="item" loading="lazy"></iframe> -->
+    </div>
   </div>
 </template>
 
@@ -8,12 +13,32 @@
 export default {
   data() {
     return {
-      loadingImg: require('assets/loading.gif')
+      loadingImg: require('assets/loading.gif'),
+      imgList: []
+    }
+  },
+  mounted() {
+    this.initLoad()
+  },
+  methods: {
+    initLoad() {
+      for (let index = 1; index <= 13; index++) {
+        if (index >= 6) {
+          this.imgList.push(require(`assets/demo${index}.jpg`))
+        } else {
+          this.imgList.push(require(`assets/demo${index}.png`))
+        }
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.img-box {
+  img {
+    width: 600px;
+    height: 600px;
+  }
+}
 </style>
