b75fd9f90ca911edd7292f6294690030d7349767
guojun <985531883@qq.com>
2020-10-01 20:53:32 +0800

    feat: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
   extends: ['@vue/standard', 'plugin:vue/strongly-recommended'],
   rules: {
     // ...other codes
-    // 'prettier/prettier': 'error',
-    trailingComma: 1, // 多行使用拖尾逗号（默认none）
+    'prettier/prettier': 'error',
   },
   parserOptions: {
     parser: 'babel-eslint',
